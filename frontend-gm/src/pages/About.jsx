5c452bad402eff49106aabc04dbe8c4744b01197
Gichuki Muchiri <collinsgichuki01@gmail.com>
2024-12-08 00:20:34 +0300

    feat: Add About & Contact Pages

--- frontend-gm/src/pages/About.jsx
+++ frontend-gm/src/pages/About.jsx
@@ -1,9 +1,77 @@
 // import React from 'react'
+import Newsletter from "../components/Newsletter";
+import Title from "../components/Title";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={" US"} />
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img className="w-full md:max-w-[450px]" src="/Hero-2.jpg" alt="" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            nisi voluptatibus nulla culpa odit velit dolorum, doloribus eius
+            ipsum dolorem minus nesciunt facilis, tempore sed inventore
+            accusantium omnis molestiae alias consequatur quia praesentium
+            veritatis numquam deserunt? Nemo voluptatum itaque modi accusamus
+            amet animi molestias officiis doloremque. Laudantium voluptates
+            eveniet aperiam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+            incidunt nihil exercitationem reiciendis voluptate, asperiores
+            corporis quas earum quam odit ad, explicabo sunt officiis ratione
+            dolores itaque enim maxime. Nobis iste magni velit dolores eius
+            quisquam modi laudantium, placeat, animi at ipsa aperiam quod,
+            ducimus voluptatem exercitationem veritatis consequatur nisi
+            voluptate magnam minima repellat odit.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consectetur aliquam, doloribus officiis adipisci tempore voluptate,
+            ex maiores quasi voluptates animi temporibus in aperiam?
+            Reprehenderit nostrum sed architecto error aliquid fugit!
+          </p>
+        </div>
+      </div>
+
+      <div className="text-xl py-4">
+        <Title text1={"WHY "} text2={"CHOOSE US"} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+            non voluptatum, quae quidem commodi minus?
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convinience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+            non voluptatum, quae quidem commodi minus?
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+            non voluptatum, quae quidem commodi minus?
+          </p>
+        </div>
+      </div>
+
+    <Newsletter />
+
+    </div>
+  );
+};
+
+export default About;
--- frontend-gm/src/pages/Contact.jsx
+++ frontend-gm/src/pages/Contact.jsx
@@ -1,9 +1,40 @@
 // import React from 'react'
 
+import Newsletter from "../components/Newsletter";
+import Title from "../components/Title";
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"Contact "} text2={"US"} />
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-[480px]" src="/Hero-2.jpg" alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            8928 Gichuki Lane <br /> Suite 123, Nanyuki, Kenya{" "}
+          </p>
+          <p className="text-gray-500">
+            {" "}
+            Tel: (254) 723-334-555 <br /> Email: admin@miricoricoli.com{" "}
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Mirico Ri Coli
+          </p>
+          <p className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <Newsletter />
+    </div>
+  );
+};
+
+export default Contact;
